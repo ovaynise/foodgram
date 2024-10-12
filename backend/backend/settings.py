3457e3c51c0151d6400a4b09ac4cd90f3b1fabc2
el <apply.powder.0p@icloud.com>
2024-10-12 15:41:20 +0300

    push

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,11 +149,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.RegularUser'
 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'https://edagram.zapto.org',
-]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
