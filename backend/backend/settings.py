7fbd9704db151f7e670c5f07c40f9392538c9c23
el <apply.powder.0p@icloud.com>
2024-10-12 16:48:43 +0300

    РАБОЧАЯ ВЕРСИЯ РЕЛИЗ 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
 }
 
 
-# Password validation
+# Password validation!
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
