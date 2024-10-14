2261978eec3fdfc1f1f72b14b48e5925264215f1
el <apply.powder.0p@icloud.com>
2024-10-14 13:47:05 +0300

    test2

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,41 +1,43 @@
 from io import BytesIO
 
-from django.db.models import Sum
 from django.http import FileResponse
+from recipes.models import IngredientRecipe, ShoppingCart
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-
-from recipes.models import IngredientRecipe
-
 pdfmetrics.registerFont(TTFont('Arial',
                                'core/fonts/ArialRegular.ttf'))
+from django.db.models import Sum
 
 
 class DownloadShopCartView(APIView):
-    """Создает список покупок в текстовый файл."""
+    """Функция создает список покупок в текстовый файл."""
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         user = request.user
-        ingredients = (IngredientRecipe.objects
-                       .filter(recipe__shoppingcart__user=user)
-                       .values('ingredient__name', 'ingredient__measurement_unit')
-                       .annotate(total_amount=Sum('amount'))
-                       .order_by('ingredient__name'))
+        shopping_cart_items = ShoppingCart.objects.filter(
+            user=user).values_list('recipe', flat=True)
+
+        ingredients = IngredientRecipe.objects.filter(
+            recipe__in=shopping_cart_items
+        ).values(
+            'ingredient__name', 'ingredient__measurement_unit'
+        ).annotate(
+            total_amount=Sum('amount')
+        )
+
         lines = ['Список ингредиентов:\n']
-        for ingredient in ingredients:
+        for count, ingredient in enumerate(ingredients, 1):
             lines.append(
-                f'{ingredient["ingredient__name"]}: '
-                f'{ingredient["total_amount"]} '
-                f'{ingredient["ingredient__measurement_unit"]}\n'
+                f"{count}. {ingredient['ingredient__name']}: "
+                f"{ingredient['total_amount']} {ingredient['ingredient__measurement_unit']}\n"
             )
 
         buffer = BytesIO()
         buffer.write(''.join(lines).encode('utf-8'))
         buffer.seek(0)
-
         return FileResponse(buffer, as_attachment=True,
                             filename='shopping_cart.txt')
